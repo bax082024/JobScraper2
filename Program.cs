b2760f5b21a0669290da2e3c8154cd8fa56fd6c8
bax082024 <bax082024@gmail.com>
2024-10-27 22:14:59 +0100

    added launch browser

--- Program.cs
+++ Program.cs
@@ -7,5 +7,10 @@ class Program
   static async Task Main(string[] args)
   {
     string url = "https://www.finn.no/job/fulltime/search.html?location=2.20001.22046.20220&q=it";
+
+    Console.WriteLine("Opening IT-related job listings in Bergen...");
+
+    using var playwright = await Playwright.CreateAsync();
+    await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false});
   }
 }
--- obj/Debug/net8.0/JobScraper2.AssemblyInfo.cs
+++ obj/Debug/net8.0/JobScraper2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobScraper2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7fb4a06b36a50db213205c8a02ddf33496163eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d88fd4036eb078f735688ab3c367d234e21fdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobScraper2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobScraper2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
