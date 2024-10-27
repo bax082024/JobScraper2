e3d88fd4036eb078f735688ab3c367d234e21fdc
bax082024 <bax082024@gmail.com>
2024-10-27 21:56:32 +0100

    first commit

--- obj/Debug/net8.0/JobScraper2.AssemblyInfo.cs
+++ obj/Debug/net8.0/JobScraper2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobScraper2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9414fa67a307b26b8fabb61dd129b48b9a39c358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7fb4a06b36a50db213205c8a02ddf33496163eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobScraper2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobScraper2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
