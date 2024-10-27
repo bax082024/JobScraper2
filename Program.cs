d7fb4a06b36a50db213205c8a02ddf33496163eb
bax082024 <bax082024@gmail.com>
2024-10-27 21:54:59 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+class Program
+{
+  static async Task Main(string[] args)
+  {
+    string url = "https://www.finn.no/job/fulltime/search.html?location=2.20001.22046.20220&q=it";
+  }
+}
